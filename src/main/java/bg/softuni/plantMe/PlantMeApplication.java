4a0fd59ce9c88adb65ad5720ce8fbbdf441d4e30
kendata1 <kendata123@gmail.com>
2024-07-05 10:44:56 +0300

    Creating project

---
+++ src/main/java/bg/softuni/plantMe/PlantMeApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.plantMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantMeApplication.class, args);
+	}
+
+}
