f96366b7dfb4d6156b9d1d612959e7c5b42dd620
kendata1 <kendata123@gmail.com>
2024-07-05 11:27:53 +0300

    Created user entity

---
+++ src/main/java/bg/softuni/plantMe/BaseEntity.java
@@ -0,0 +1,19 @@
+package bg.softuni.plantMe;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    public BaseEntity () {}
+
+    public long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/bg/softuni/plantMe/User.java
@@ -0,0 +1,58 @@
+package bg.softuni.plantMe;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity(name = "users")
+public class User extends BaseEntity{
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
